da1192e405a0545f6ac94acea7ed4effd41cdb73
Zaeem104158 <zaeemhasan007@gmail.com>
2025-06-25 17:48:55 +0600

    Datasourse of profile is complete

---
+++ lib/src/features/profile/data/model/profile_response_model.dart
@@ -0,0 +1,50 @@
+import 'package:point_of_sale/src/features/profile/domain/entity/profile_response_entity.dart';
+
+class ProfileResponseModel extends ProfileResponseEntity {
+  const ProfileResponseModel({
+    super.login,
+    super.name,
+    super.avatarUrl,
+    super.publicRepos,
+    super.followers,
+    super.following,
+    super.createdAt,
+    super.updatedAt,
+  });
+
+  factory ProfileResponseModel.fromJson(Map<String, dynamic> json) {
+    return ProfileResponseModel(
+      login: json['login'],
+
+      name: json['name'],
+
+      publicRepos: json['public_repos'],
+
+      followers: json['followers'],
+      following: json['following'],
+      createdAt: json['created_at'] == null
+          ? null
+          : DateTime.parse(json['createdAt']),
+      updatedAt: json['updated_at'] == null
+          ? null
+          : DateTime.parse(json['updated_at']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'login': login,
+
+      'avatar_url': avatarUrl,
+
+      'name': name,
+
+      'public_repos': publicRepos,
+
+      'followers': followers,
+      'following': following,
+      'created_at': createdAt?.toIso8601String(),
+      'updated_at': updatedAt?.toIso8601String(),
+    };
+  }
+}
---
+++ lib/src/features/profile/domain/entity/profile_response_entity.dart
@@ -0,0 +1,22 @@
+class ProfileResponseEntity {
+  final String? login;
+  final String? avatarUrl;
+  final String? name;
+
+  final int? publicRepos;
+  final int? followers;
+  final int? following;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+
+  const ProfileResponseEntity({
+    this.login,
+    this.avatarUrl,
+    this.name,
+    this.publicRepos,
+    this.followers,
+    this.following,
+    this.createdAt,
+    this.updatedAt,
+  });
+}
